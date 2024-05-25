f61ca51f3af6a347be19166a108a39a689c04906
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-25 14:20:42 +0300

    Mac

--- src/Components/Atom/atom.jsx
+++
@@ -1,4 +0,0 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Sphere } from '@react-three/drei';
-
--- src/Components/Computer/computer.jsx
+++ src/Components/Computer/computer.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useGLTF } from '@react-three/drei';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
 function Models() {
-  const { scene } = useGLTF('../../../Public/mac-draco.glb');
-  // Ensure materials are properly applied to the model
-  return <primitive object={scene} />;
+  const { scene } = useGLTF("../../../Public/mac-draco.glb");
+  const modelRef = useRef();
+
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.008; // Adjust rotation speed as needed
+    }
+  });
+
+  return <primitive ref={modelRef} object={scene} />;
 }
 
 function Model() {
   return (
-    <Canvas>
-      <ambientLight intensity={2} /> {/* Adjust intensity as needed */}
-      <pointLight position={[10, 10, 10]} /> {/* Add a point light */}
-      <pointLight position={[0, 5, 0]} /> {/* Add a point light */}
-      <Models />
-      <OrbitControls />
-    </Canvas>
+    <div className="flex items-center justify-center min-h-screen bg-silver">
+      <Canvas style={{ width: "100%", height: "100vh" }}>
+        <ambientLight intensity={2} /> {/* Adjust intensity as needed */}
+        <pointLight position={[10, 10, 10]} /> {/* Add a point light */}
+        <pointLight position={[0, 5, 0]} /> {/* Add a point light */}
+        <Models />
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 }
 
