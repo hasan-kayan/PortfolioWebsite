c0c58270a7cce0c5c02b16f16753d587df3252ac
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-25 14:20:42 +0300

    Mac

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Model from './Components/Computer/computer'
+import React from "react";
+import "tailwindcss/tailwind.css";
+import Model from "./Components/Computer/computer";
+
 function App() {
-
-
   return (
-    <Model/>
-  )
+
+      <Model />
+   
+  );
 }
 
-export default App
+export default App;
