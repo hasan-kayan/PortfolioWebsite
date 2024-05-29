615dfc48f787b2074cf55680d3a9f286fb53c211
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-29 14:35:36 +0300

    Init of contact modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
 import MediumProfile from "./Pages/Blogs/Blogs";
+import ContactModal from "./Components/ContactModal/ContactModal";
 
 function App() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-silver">
       <MediumProfile />
+      <ContactModal />
     </div>
   );
 }
