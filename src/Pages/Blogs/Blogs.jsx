b3389331bb93fb0244f31386b3229d8a1fdf7760
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-29 13:25:16 +0300

    Not Bad For Articles but should get better

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,51 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Parser from 'rss-parser';
+import React from "react";
 
-const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        let parser = new Parser();
-        let feed = await parser.parseURL('https://medium.com/feed/@hasankayan');
-        console.log("feed", feed.items)
-        setBlogs(feed.items);
-        setLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-
-    fetchBlogs();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+const MediumProfile = () => {
+  const articles = [
+    {
+      id: 1,
+      title: "What Is Git?",
+      imageUrl:
+        "https://miro.medium.com/v2/resize:fit:720/format:webp/0*vdkDO6bUii6g_DVY.png",
+      url: "https://hasankayan.medium.com/what-is-git-5dbe03940fc5",
+    },
+    {
+      id: 2,
+      title: "Zustand State Manager And JS Memory",
+      imageUrl:
+        "https://miro.medium.com/v2/resize:fit:720/format:webp/0*-7b-EPfP3pgn9_uI.jpg",
+      url: "https://hasankayan.medium.com/zustand-state-manager-and-js-memory-b39efe6a811e",
+    },
+    // Add more articles here...
+  ];
 
   return (
-    <div>
-      <h1>My Medium Blogs</h1>
-      <ul>
-        {blogs.map((blog, index) => (
-          <li key={index}>
-            <a href={blog.link} target="_blank" rel="noopener noreferrer">
-              {blog.title}
+
+    <div className="article-list grid grid-cols-1 gap-4">
+      <h1 className="header">
+        My Articles
+      </h1>
+      {articles.map((article) => (
+        <div className="article-box rounded overflow-hidden shadow-lg" key={article.id}>
+          <img className="w-full" src={article.imageUrl} alt={article.title} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{article.title}</div>
+            <a
+              className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
+              href={article.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read More
             </a>
-            <p>{blog.contentSnippet}</p>
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Blogs;
+export default MediumProfile;
