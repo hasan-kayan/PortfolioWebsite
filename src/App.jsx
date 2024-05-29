21901871d366e0404d5b5b08096e0103f7271405
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-29 13:25:16 +0300

    Not Bad For Articles but should get better

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
-import Blogs from "./Pages/Blogs/Blogs";
+import MediumProfile from "./Pages/Blogs/Blogs";
 
 function App() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-silver">
-      <Blogs />
+      <MediumProfile />
     </div>
   );
 }
