b92bbd0fb490628b7843a536b7eb2371e2fdf418
hasan-kayan <hasankayan2000@hotmail.com>
2024-06-28 17:32:18 +0300

    some state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom';
 import "tailwindcss/tailwind.css";
 import MediumProfile from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home";
 import ContactModal from "./Components/ContactModal/ContactModal";
-
+import Navbar from './Components/Navbar/Navbar.jsx';
 function App() {
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-silver">
-      <MediumProfile />
-
+      <div className="flex flex-col items-center justify-center min-h-screen bg-silver">
+        <Router>
+            <div>
+                <Navbar />
+                <div className="container mx-auto p-4 ">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/blogs" element={<MediumProfile />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import logo from "../../../assets/logo.png"; // Correctly import the logo
+
+const CustomNavbar = () => {
+    return (
+        <Navbar className="flex justify-center w-full bg-appbgcolor p-4">
+            <NavbarBrand className="flex items-center">
+                <img src={logo} alt="Logo" className="h-10 w-10 rounded-3xl" />
+                <p className="font-bold text-white ml-2">Hasan Kayan</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+};
+
+export default CustomNavbar;
