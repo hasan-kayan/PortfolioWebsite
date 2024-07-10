4d6abed8aae8ce7155e948707b3345b09fe5df55
hasan-kayan <hasankayan2000@hotmail.com>
2024-07-10 17:43:59 +0300

    Some chages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+
+} from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
