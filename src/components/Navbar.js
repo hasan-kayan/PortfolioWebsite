573f84831c3a0f8ca1e5f86e0ba1659233b56e91
hasan-kayan <hasankayan2000@hotmail.com>
2024-09-11 10:08:16 +0300

    Deployment and new projhect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,6 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
