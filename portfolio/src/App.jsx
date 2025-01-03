380481f03c46b0330d0458ec592fdf822b607816
hasan.kayan <hasan.kayan@asisguard.com>
2025-01-03 13:36:16 +0300

    Fit pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-appbgcolor">
+        <NavBar />
         <div className="container mx-auto p-4">
-         Hello
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+          </Routes>
         </div>
       </div>
     </Router>
