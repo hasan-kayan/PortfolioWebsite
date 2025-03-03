d86d692c6703322e22dcfe0bc578a0365c5e132d
hasan-kayan <hasankayan2000@hotmail.com>
2025-03-03 14:33:30 +0300

    Models are displaying

--- src/Pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div className="flex justify-center items-center h-screen text-4xl font-bold text-white">
-        heellooo
-      </div>
-    );
-  }
-  
