5fc2c31773ca50599bf19fd3d2859a517efbc1e8
hasan-kayan <hasankayan2000@hotmail.com>
2025-03-03 16:49:47 +0300

    Stacks glb files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
+
+//COMPONENTS
 import NavBar from "./Components/Navbar";
+//PAGES (ROUTES)
 import Home from "./Pages/Home";
 import BlogList from "./Pages/Blogs/Blogs"
 import BlogDetail from "./Pages/Blogs/BlogDetail";
-
 function App() {
   return (
     <Router>
-    <div className="min-h-screen min-w-full overflow-hidden bg-gradient-to-r from-appbgcolorstart to-appbgcolorend">
-    <NavBar />
+      <motion.div
+        className="min-h-screen min-w-full overflow-hidden"
+        animate={{
+          background: [
+            "linear-gradient(to right, #ff7eb3, #ff758c)",
+            "linear-gradient(to right, #6a11cb, #2575fc)",
+            "linear-gradient(to right, #ff7eb3, #ff758c)",
+          ],
+        }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
+      >
+        <NavBar />
         <div className="container mx-auto p-4">
-          <Routes>             
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blogs" element={<BlogList />} />
             <Route path="/blog/:id" element={<BlogDetail />} />
-            <Route path="/" element={<Home />} />
-
           </Routes>
         </div>
-      </div>
+      </motion.div>
     </Router>
   );
 }
+
 
 export default App;
