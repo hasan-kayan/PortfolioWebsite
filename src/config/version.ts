aad9e406cf9d0a2aa9ae94b93af4023c5a7b5889
hasan-kayan <hasankayan2000@hotmail.com>
2025-12-24 22:46:48 +0300

    chore: bump version to V1.0.5

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,13 +1,13 @@
 // Auto-generated version file - Do not edit manually
 // This file is updated automatically by deploy.sh
 
-export const APP_VERSION = 'V1.0.4';
-export const APP_VERSION_NUMBER = '1.0.4';
+export const APP_VERSION = 'V1.0.5';
+export const APP_VERSION_NUMBER = '1.0.5';
 
 // Version info for display
 export const versionInfo = {
-  version: 'V1.0.4',
+  version: 'V1.0.5',
   major: 1,
   minor1: 0,
-  minor2: 4,
+  minor2: 5,
 };
